ed1bc901f056e1fe5a1083f02749b8f3af705806
DilekaRatnayaka <dilekaratnayaka@gmail.com>
2024-06-04 10:02:18 +0530

    Create hello world Express.js application

---
+++ Node.js
@@ -0,0 +1,13 @@
+const express = require('express'); // Import the Express.js module
+const app = express(); // Create an instance of an Express application
+const port = process.env.PORT || 3000; // Set the port from an environment variable or default to 3000
+
+// Define a route handler for the root URL ('/')
+app.get('/', (req, res) => {
+  res.send('Hello, world!'); // Send "Hello, world!" as the response
+});
+
+// Start the server and listen on the specified port
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`); // Log a message to the console
+});
